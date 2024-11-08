d41b05b6b8224bebc4aa3b89b6a984f746239d75
ruan <ruanlazareti@hotmail.com>
2024-11-08 15:01:47 -0300

    adjusted game model

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-		protected $fillable =  ['status'];
+		protected $fillable =  ['current_player_id'];
     public function players(): \Illuminate\Database\Eloquent\Relations\HasMany
     {
         return $this->hasMany(Player::class);
