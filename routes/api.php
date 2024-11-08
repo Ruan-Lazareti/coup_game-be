90af00d57d075a4092ba2e294058065187b49cad
ruan <ruanlazareti@hotmail.com>
2024-11-08 15:01:23 -0300

    added new route to game functions

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,16 @@ use App\Http\Controllers\GameController;
 	Route::resource('/cards', CardController::class);
   Route::get('/perks', [PerkController::class, 'index']);
 
+	//Rotas pra iniciar o jogo
 	Route::group(['middleware' => ['web']], function () {
 		Route::post('/game/create', [GameController::class, 'createGame']);
 		Route::get('/game/players', [GameController::class, 'getPlayers']);
 		Route::post('/game/join', [GameController::class, 'joinGame']);
 		Route::post('/game/start', [GameController::class, 'startGame']);
 		Route::get('/game/player-cards', [GameController::class, 'getPlayerCards']);
+	});
+
+	//Rotas de funcionalidade do jogo
+	Route::group(['middleware' => ['web']], function () {
+		Route::get('/game/income', [GameController::class, 'income']);
 	});
